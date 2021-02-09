b59c94fb4b7908be75613ccba5a331c6d6153676
Hasan-star-creat <74924051+Hasan-star-creat@users.noreply.github.com>
2021-02-09 20:03:34 +0600

    food detaisl list added

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
  const searcFood= () => {
  const textFood =  document.getElementById('search-field').value;
   const url =`https://www.themealdb.com/api/json/v1/1/filter.php?c=${textFood}`;
-  fetch(url)
-  .then(res => res.json())
-  .then(data => displayFood(data.meals))
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayFood(data.meals))
  }
 
  const displayFood = foods => {
       const foodContainer = document.getElementById('food-name');
       // foodContainer.innerText = ''; // previw data don't store
       foods.forEach(food => {
-         console.log(food)
           const divFood = document.createElement('div');
+           
            divFood.className = 'my-3 p-3 d-flex';
           divFood.innerText = food.strMeal;
           divFood.innerHTML = `
-       <div class="foodStyle">
+       <div onclick = "foodDisplyDetails('${food.idMeal}')" class="foodStyle">
        <img src="${food.strMealThumb}"> 
        <h5>${food.strMeal}</h5> 
    </div> `
-  //  <div class="col-md-3 text-md-right text-center">
-  //      <button onclick = "getLyric('${song.artist.name}', '${song.title}')" class="btn btn-success">Get Lyrics</button>
-  //  </div> 
-          foodContainer.appendChild(divFood);
+         foodContainer.appendChild(divFood);
       });
  }
+
+  // food display details 
+   const foodDisplyDetails = (idmeal) => {
+      const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idmeal}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(data => displayDetails(data.meals))
+   }
+    
+   const displayDetails = (food) => {
+    const detaisfood = [food[0].strIngredient1, food[0].strIngredient3, food[0].strIngredient4, food[0].strIngredient5, food[0].strIngredient6, food[0].strIngredient7];
+     const divFoodDetails = document.getElementById('food-details');
+     detaisfood.forEach(details => {
+        const li = document.createElement('li');     
+        li.innerText = details;
+        divFoodDetails.appendChild(li)
+      });
+   
+
+            
+            console.log('Ingredients', food);
+            console.log('area', food[0].strIngredient1);
+            console.log('area', food[0].strIngredient1);
+
+   }
